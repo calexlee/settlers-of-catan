16d78c2c97a7773b63cd130e809f91a6a30d270d
Alex <cal368@cornell.edu>
2019-10-27 14:08:56 -0400

    Implemented MLI for Node
    Added types for node

--- node.mli
+++ node.mli
@@ -1,1 +1,20 @@
-(* Consists of attributes of an index that represents what node it is on the board City/settlements located there and 3 neighboring tiles.*)
+module type Node = sig 
+
+  (**The type of a settlement *)
+  type s
+
+  (**The type of a Node *)
+  type t = {
+    neigh_Tiles: Tile.t list;
+    settlement : s;
+    (**Player : player.player *) 
+    index: int;
+    edges: Edge.t list;
+  }
+
+  (**[give_resource dr] changes the resources of the players who have
+     settlements adjacent to the tiles with the number [dr]*)
+  val give_resource : int -> unit
+
+
+end
