b2c4253f693a06bc7f6039c274a653b31e58233b
xx284 <xx284@cornell.edu>
2019-11-01 16:00:28 -0400

    update board.ml and board.mli

--- board.ml
+++ board.ml
@@ -4,9 +4,16 @@ type t = Tile.t list
 
 let initial_board = 
   failwith "unimpelmented"
+
 let rand_board = 
   failwith "unimpelmented"
-let get_tile = 
-  failwith "unimpelmented"
-let get_tiles_with_num = 
-  failwith "unimpelmented"
+
+let get_tile n t = 
+  List.nth n t
+
+let get_tiles_with_num board n = 
+  match board with
+  | [] -> []
+  | h::t -> if (h.number = n) then h :: get_tiles_with_num t n 
+  else get_tiles_with_num t n
+
--- board.mli
+++ board.mli
@@ -3,13 +3,13 @@
 type t
 
 (**[initial_board] becomes a pre-built standard board of type t*)
-val initial_board : unit->t
+val initial_board : unit -> t
 
 (**[rand_board] becomes a randomized board of type t*)
-val rand_board :unit->t
+val rand_board : unit -> t
 
 (**[get_tile] is a the type located at an index*)
-val get_tile : int -> Tile.t
+val get_tile : t -> int -> Tile.t
 
 (**[get_tiles_with_num] is the list of tiles with a certain int *)
-val get_tiles_with_num : int -> Tile.t list
+val get_tiles_with_num : t -> int -> Tile.t list
