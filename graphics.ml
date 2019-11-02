80911bc518cd51faf6b7248a93f45578116753ad
Alex <cal368@cornell.edu>
2019-11-01 22:21:05 -0400

    Graphics Number Grabbing
    Implemented getting numbers to draw for graphics

--- graphics.ml
+++ graphics.ml
@@ -2,6 +2,8 @@
 
 let grab_resource t n =     
   List.nth t n |> Tile.get_resource
+let grab_num t n = 
+  List.nth t n |> Tile.get_number
 
 let draw_board t = 
   print_string("                                  >-----< \n");
@@ -12,31 +14,31 @@ let draw_board t =
   print_string("                       /~~~~~~~~~\\*~~~~~*/~~~~~~~~~\\ \n");
   print_string("                >-----<~~~~~~~~~~~>-----<~~~~~~~~~~~>-----< \n");
   print_string("               /~~~~~~~\\~~~~~~~~~/       \\~~~~~~~~~/~~~~~~~\\ \n");
-  print_string("              /~~~2:1~~~\\~~~~~~~/    #    \\~~~~~~~/~~~2:1~~~\\ \n");
+  print_string("              /~~~2:1~~~\\~~~~~~~/    ");print_string(string_of_int (grab_num t 0));print_string("    \\~~~~~~~/~~~2:1~~~\\ \n");
   print_string("       >-----<~~~sheep~~~*>-----<   ");print_string(grab_resource t 0);print_string("    >-----<*~~rock~~~>-----< \n");
   print_string("      /~~~~~~~\\~~~~~~~~~/       \\         /       \\~~~~~~~~~/~~~~~~~\\ \n");
-  print_string("     /~~~~~~~~~\\~~~~~~*/    #    \\       /    #    \\*~~~~~~/~~~~~~~~~\\ \n");
+  print_string("     /~~~~~~~~~\\~~~~~~*/    ");print_string(string_of_int (grab_num t 1));print_string("    \\       /    ");print_string(string_of_int (grab_num t 2));print_string("    \\*~~~~~~/~~~~~~~~~\\ \n");
   print_string("    <~~~~~~~~~~~>-----<   ");print_string(grab_resource t 1);print_string("    >-----<  ");print_string(grab_resource t 2);print_string("   >-----<~~~~~~~~~~~> \n");
   print_string("     \\~~~~~~~~~/       \\         /       \\         /       \\~~~~~~~~~/ \n");
-  print_string("      \\~~~~~~~/    #    \\       /    #    \\       /    #    \\~~~~~~~/ \n");
+  print_string("      \\~~~~~~~/    ");print_string(string_of_int (grab_num t 3));print_string("    \\       /    ");print_string(string_of_int (grab_num t 4));print_string("    \\       /    ");print_string(string_of_int (grab_num t 5));print_string("    \\~~~~~~~/ \n");
   print_string("       >-----<  ");print_string(grab_resource t 3);print_string("   >-----<   ");print_string(grab_resource t 4);print_string("    >-----<   ");print_string(grab_resource t 5);print_string("    >-----< \n");
   print_string("      /~~~~~~~\\         /       \\         /       \\         /~~~~~~~\\ \n");
-  print_string("     /~~~2:1~~~\\       /    #    \\       /         \\       /~~~2:1~~~\\ \n");
-  print_string("    <~~~wood~~* >-----<  ");print_string(grab_resource t 6);print_string("     >-----<     #    >-----<*~~ore~~~~> \n");
+  print_string("     /~~~2:1~~~\\       /    ");print_string(string_of_int (grab_num t 6));print_string("    \\       /         \\       /~~~2:1~~~\\ \n");
+  print_string("    <~~~wood~~* >-----<  ");print_string(grab_resource t 6);print_string("     >-----<     ");print_string(string_of_int (grab_num t 7));print_string("    >-----<*~~ore~~~~> \n");
   print_string("     \\~~~~~~~~~/       \\         /       \\  ");print_string(grab_resource t 7);print_string(" /       \\~~~~~~~~~/ \n");
-  print_string("      \\~~~~~~*/    #    \\       /    #    \\       /    #    \\*~~~~~~/ \n");
+  print_string("      \\~~~~~~*/    ");print_string(string_of_int (grab_num t 8));print_string("    \\       /    ");print_string(string_of_int (grab_num t 9));print_string("    \\       /    ");print_string(string_of_int (grab_num t 10));print_string("    \\*~~~~~~/ \n");
   print_string("       >-----<  ");print_string(grab_resource t 8);print_string("    >-----<   ");print_string(grab_resource t 9);print_string("   >-----<   ");print_string(grab_resource t 10);print_string("    >-----< \n");
   print_string("      /~~~~~~~\\         /       \\         /       \\         /~~~~~~~\\ \n");
-  print_string("     /~~~~~~~~~\\       /    #    \\       /    #    \\       /~~~~~~~~~\\ \n");
+  print_string("     /~~~~~~~~~\\       /    ");print_string(string_of_int (grab_num t 11));print_string("    \\       /    ");print_string(string_of_int (grab_num t 12));print_string("    \\       /~~~~~~~~~\\ \n");
   print_string("    <~~~~~~~~~~~>-----<  ");print_string(grab_resource t 11);print_string("   >-----<   ");print_string(grab_resource t 12);print_string("    >-----<~~~~~~~~~~~>  \n");
   print_string("     \\~~~~~~~~~/       \\         /       \\         /       \\~~~~~~~~~/ \n");
-  print_string("      \\~~~~~~~/    #    \\       /    #    \\       /    #    \\~~~~~~~/ \n");
+  print_string("      \\~~~~~~~/    ");print_string(string_of_int (grab_num t 13));print_string("    \\       /    ");print_string(string_of_int (grab_num t 14));print_string("    \\       /    ");print_string(string_of_int (grab_num t 15));print_string("    \\~~~~~~~/ \n");
   print_string("       >-----<   ");print_string(grab_resource t 13);print_string("   >-----<   ");print_string(grab_resource t 14);print_string("    >-----<   ");print_string(grab_resource t 15);print_string("   >-----< \n");
   print_string("      /~~~~~~*\\         /       \\         /       \\         /*~~~~~~\\ \n");
-  print_string("     /~~~~~~~~~\\       /    #    \\       /    #    \\       /~~~~~~~~~\\ \n");
+  print_string("     /~~~~~~~~~\\       /    ");print_string(string_of_int (grab_num t 16));print_string("    \\       /    ");print_string(string_of_int (grab_num t 17));print_string("    \\       /~~~~~~~~~\\ \n");
   print_string("    <~~~~3:1~~~*>-----<   ");print_string(grab_resource t 16);print_string("   >-----<   ");print_string(grab_resource t 17);print_string("    >-----<*~~~3:1~~~~> \n");
   print_string("     \\~~~~~~~~~/~~~~~~~\\         /       \\         /~~~~~~~\\~~~~~~~~~/ \n");
-  print_string("      \\~~~~~~~/~~~~~~~~~\\       /    #    \\       /~~~~~~~~~\\~~~~~~~/ \n");
+  print_string("      \\~~~~~~~/~~~~~~~~~\\       /    ");print_string(string_of_int (grab_num t 18));print_string("    \\       /~~~~~~~~~\\~~~~~~~/ \n");
   print_string("       >-----< ~~~~~~~~~~>-----<   ");print_string(grab_resource t 18);print_string("   >-----<~~~~~~~~~~~>-----< \n");
   print_string("              \\~~~~~~~~~/*~~~~~*\\         /*~~~~~*\\~~~~~~~~~/ \n");
   print_string("               \\~~~~~~~/~~~~~~~~~\\       /~~~2:1~~~\\~~~~~~~/ \n");
