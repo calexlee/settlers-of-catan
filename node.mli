9aee190bc521ebbe25cd95a0eaa3a48732ee2909
zcc6 <zcc6@cornell.edu>
2019-11-05 15:19:27 -0500

    Update node.mli

--- node.mli
+++ node.mli
@@ -36,5 +36,5 @@ val get_player : t -> Player.t
 (**[get_settlement t] is the settlement in [t] *)
 val get_settlement : t -> string
 
-(**[generateNodes ()] is the *)
+(**[generateNodes ()] is a generated list of nodes indexed from 1-54 *)
 val generateNodes : unit -> t list
